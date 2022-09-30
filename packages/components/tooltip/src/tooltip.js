033c5029e26ab556cefdfb30b0c0cf62200f2ffe
Hettlingen <martinbraun@scoop.ch>
2022-09-30 10:45:37 +0200

    new tooltip icons

--- packages/components/tooltip/src/tooltip.js
+++ packages/components/tooltip/src/tooltip.js
@@ -1,7 +1,9 @@
 const template = document.createElement('template');
 
 template.innerHTML = `
-<!--    <link rel="stylesheet" type="text/css" href="tooltip.css">-->
+<!--    <style>-->
+<!--        @import "/css/components/card.css";-->
+<!--    </style>-->
   
     <style>
         .container {
@@ -31,32 +33,41 @@ template.innerHTML = `
         svg {
             width: 1rem;
             cursor: pointer;
+            color: darkorange;
         }
     </style>
 
     <div class="container">
-        <svg id="icon-info" viewBox="0 0 174 174" class="icon-info">
-          <defs>
-            <clipPath id="clip-path">
-              <rect id="Rechteck_148" data-name="Rechteck 148" width="174" height="174" fill="none"/>
-            </clipPath>
-          </defs>
-          <g id="Gruppe_187" data-name="Gruppe 187" clip-path="url(#clip-path)">
-            <path id="Pfad_4008" data-name="Pfad 4008" d="M78.5,43.609H95.939V61.052H78.5Zm0,34.887H95.939v52.33H78.5ZM87.217,0a87.217,87.217,0,1,0,87.217,87.217A87.249,87.249,0,0,0,87.217,0m0,156.992a69.774,69.774,0,1,1,69.774-69.774,69.867,69.867,0,0,1-69.774,69.774"/>
+        <svg id="icon-info" viewBox="0 0 50 50" class="icon-info">
+          <g id="Ellipse_52" data-name="Ellipse 52" stroke="currentColor" stroke-width="1">
+            <circle cx="25" cy="25" r="25" stroke="none"/>
+            <circle cx="25" cy="25" r="24.5" fill="currentColor"/>
+          </g>
+          <g id="Rechteck_161" data-name="Rechteck 161" transform="translate(21 20.742)" fill="#fff" stroke="#fff" stroke-width="1">
+            <rect width="8" height="20" stroke="none"/>
+            <rect x="0.5" y="0.5" width="7" height="19" fill="none"/>
+          </g>
+          <g id="Rechteck_162" data-name="Rechteck 162" transform="translate(21 9.258)" fill="#fff" stroke="#fff" stroke-width="1">
+            <rect width="8" height="8" stroke="none"/>
+            <rect x="0.5" y="0.5" width="7" height="7" fill="none"/>
           </g>
         </svg>
         
-        <svg id="icon-cancel" viewBox="0 0 150 150" class="icon-close">
-          <defs>
-            <clipPath id="clip-path">
-              <rect id="Rechteck_147" data-name="Rechteck 147" width="150" height="150" fill="none"/>
-            </clipPath>
-          </defs>
-          <g id="Gruppe_185" data-name="Gruppe 185" clip-path="url(#clip-path)">
-            <path id="Pfad_4007" data-name="Pfad 4007" d="M94.27,44.926,74.877,64.319,55.484,44.926,44.926,55.484,64.319,74.877,44.926,94.27l10.558,10.558L74.877,85.435,94.27,104.828,104.828,94.27,85.435,74.877l19.393-19.393ZM74.877,0a74.877,74.877,0,1,0,74.877,74.877A74.808,74.808,0,0,0,74.877,0m0,134.779a59.9,59.9,0,1,1,59.9-59.9,59.981,59.981,0,0,1-59.9,59.9" transform="translate(0.246)"/>
+        <svg id="icon-close" viewBox="0 0 50 50" class="icon-close">
+          <g id="Ellipse_52" data-name="Ellipse 52" stroke="currentColor" stroke-width="1">
+            <circle cx="25" cy="25" r="25" stroke="none"/>
+            <circle cx="25" cy="25" r="24.5" fill="currentColor"/>
+          </g>
+          <g id="Pfad_4014" data-name="Pfad 4014" transform="translate(33.963 11.359) rotate(45)" fill="#fff">
+            <path d="M -0.05643713474273682 30.92506980895996 L 0.4912384748458862 0.491237998008728 L 7.349330425262451 0.3678223788738251 L 6.801654815673828 30.80165481567383 L -0.05643713474273682 30.92506980895996 Z" stroke="none"/>
+            <path d="M 6.840084552764893 0.8770675659179688 L 0.9824800491333008 0.9824790954589844 L 0.4528088569641113 30.41582489013672 L 6.310413360595703 30.3104133605957 L 6.840084552764893 0.8770675659179688 M 7.858576774597168 -0.1414241790771484 L 7.292896747589111 31.29289627075195 L -0.5656833648681641 31.43431663513184 L -3.337860107421875e-06 -3.814697265625e-06 L 7.858576774597168 -0.1414241790771484 Z" stroke="none" fill="#fff"/>
+          </g>
+          <g id="Pfad_4015" data-name="Pfad 4015" transform="translate(11.336 16.816) rotate(-45)" fill="#fff">
+            <path d="M 7.915015697479248 31.06649208068848 L 1.056923747062683 30.94307708740234 L 0.5092481374740601 0.5092446804046631 L 7.367340087890625 0.6326602697372437 L 7.915015697479248 31.06649208068848 Z" stroke="none"/>
+            <path d="M 1.018494129180908 1.018489837646484 L 1.548165321350098 30.45183563232422 L 7.405769824981689 30.55724716186523 L 6.8760986328125 1.1239013671875 L 1.018494129180908 1.018489837646484 M 1.9073486328125e-06 -1.9073486328125e-06 L 7.858582019805908 0.14141845703125 L 8.424262046813965 31.57573890686035 L 0.5656819343566895 31.43431854248047 L 1.9073486328125e-06 -1.9073486328125e-06 Z" stroke="none" fill="#fff"/>
           </g>
         </svg>
-        
+         
         <div class="container-tooltip">
             <slot name="message"/>
         </div>
