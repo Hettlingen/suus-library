b5c4e381edd7ff283e5c6cd69dc4b82a33b0ef53
Hettlingen <martinbraun@scoop.ch>
2022-10-02 12:58:46 +0200

    modal dialog

--- packages/modal/src/modal.js
+++ packages/modal/src/modal.js
@@ -2,30 +2,65 @@ const modalTemplate = document.createElement('template');
 modalTemplate.innerHTML = `
     <style>
         dialog {
-            background: grey;
+            display: grid;
+            position: fixed;
+            overflow: hidden;
+            margin: auto;
+            padding: 2rem;
+            z-index: 6;
+            box-shadow: 5px 5px 10px rgba(0,0,0,.1);
+            border-radius: .5em;
+        }
+        
+        dialog:not([open]) {
+            display: none;
+        }
+
+        dialog[open] { 
+            width: 20em; 
+            background: white; 
+            border: thin solid lightgray;
+            margin: 5em auto;
+        }
+              
+        /* blurs the background of the window background */
+        dialog::backdrop {
+            backdrop-filter: blur(25px);
+        }
+        
+        .button-close {
+            font-size: 30px;
+            color: black;
+            cursor: pointer;
         }
     </style>
   
-    <dialog>
-        Inhalt des Dialogs
+    <dialog id="idDialog">
+        <header>
+            <span id="idButtonClose" class="button-close">&times;</span>
+            <h1>Titel</h1>                    
+        </header>        
+
+        <p>Inhalt des Dialogs</p>
+        
+        <footer>
+            <button id="idButtonCancel" value="cancel" autofocus>Cancel</button>
+            <button id="idButtonConfirm" value="confirm">Confirm</button>
+        </footer>
     </dialog>
 `;
 
 class Modal extends HTMLElement {
 
-    static get observedAttributes() {
-        return ['key'];
-    }
-
     constructor() {
         super();
-        this.showInfo = false;
         this.attachShadow({ mode: 'open' });
         this.shadowRoot.appendChild(modalTemplate.content.cloneNode(true));
-    }
 
-    connectedCallback() {
-        this.shadowRoot.querySelector('#close').addEventListener('click', () => {this.remove()});
+        this.dialog = this.shadowRoot.getElementById("idDialog");
+        this.buttonConfirm = this.shadowRoot.getElementById("idButtonConfirm");
+        this.buttonCancel = this.shadowRoot.getElementById("idButtonCancel");
+        this.buttonClose = this.shadowRoot.getElementById("idButtonClose");
     }
 
     /**
@@ -33,6 +68,15 @@ class Modal extends HTMLElement {
      */
     connectedCallback() {
         console.log('Lifecycle methode: Custom square element added to page.');
+        this.buttonConfirm.addEventListener("click", () => {
+            // TODO do something
+        });
+        this.buttonCancel.addEventListener("click", () => {
+            this.dialog.close();
+        });
+        this.buttonClose.addEventListener("click", () => {
+            this.dialog.close();
+        });
     }
 
     /**
@@ -45,8 +89,20 @@ class Modal extends HTMLElement {
     /**
      * Lifecycle methode: This method is called when any attribute to the custom element changes
      */
-    attributeChangedCallback(name, oldValue, newValue) {
+    attributeChangedCallback(nameAttribute, oldValue, newValue) {
         console.log('Lifecycle methode: Custom button element attributes changed.');
+        switch (nameAttribute) {
+            case "show": {
+                newValue == 'true'
+                    ? this.dialog.showModal()
+                    : this.dialog.close();
+                break;
+            }
+            default: {
+                console.log("unhandled attribute change", nameAttribute, oldValue, newValue);
+                break;
+            }
+        }
     }
 
     /**
@@ -54,7 +110,7 @@ class Modal extends HTMLElement {
      */
     static get observedAttributes() {
         console.log('Lifecycle methode: Attributes to observe.');
-        // return ['label'];
+        return ['show'];
     }
 }
 
